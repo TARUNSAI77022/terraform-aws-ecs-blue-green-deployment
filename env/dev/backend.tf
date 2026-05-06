ace8539706231d58218bcec95337f6461ad6e83d
tarunsai <67566514+TARUNSAI77022@users.noreply.github.com>
2026-05-06 16:14:44 +0530

    add Terraform CI/CD pipeline, backend configuration, and IAM documentation

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  # backend "s3" {
-  #   bucket         = "your-terraform-state-bucket"
-  #   key            = "dev/terraform.tfstate"
-  #   region         = "ap-south-1"
-  #   encrypt        = true
-  #   dynamodb_table = "terraform-state-lock"
-  # }
+  backend "s3" {
+    bucket         = "terraform-state-inti-ruchi"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
 }
